0b374a9e86262fa4d18cdb03d10ec69cd24b23b6
James Ralphs <jr39.dev@gmail.com>
2015-07-24 21:38:41 +0200

    demo throwing different kinds of errors

---
+++ src/user.go
@@ -0,0 +1,32 @@
+package app_user
+
+import "fmt"
+
+type UnauthorisedError struct{}
+
+func (e *UnauthorisedError) Error() string {
+	return "you are unathorised"
+}
+
+type TokenTooLongError struct {
+	length int
+}
+
+func (e *TokenTooLongError) Error() string {
+	return fmt.Sprintf("token too long - they should be max 16 characters and this one is %i characters", e.length)
+}
+
+type User struct {
+	username string
+}
+
+func AuthenticateByToken(authtoken string) (*User, error) {
+	validToken := "johns_token"
+	if 16 < len(authtoken) {
+		return nil, &TokenTooLongError{length: len(authtoken)}
+	}
+	if validToken != authtoken {
+		return nil, &UnauthorisedError{}
+	}
+	return &User{username: "john"}, nil
+}
---
+++ src/user_test.go
@@ -0,0 +1,43 @@
+package app_user
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAuthenticateWithValidToken(t *testing.T) {
+
+	validToken := "johns_token"
+	user, err := AuthenticateByToken(validToken)
+	assert.Equal(t, nil, err, "there should be no error when using 'johns_token' as the token")
+	assert.Equal(t, "john", user.username, "username should be john")
+}
+
+func TestAuthenticateWithTooLongToken(t *testing.T) {
+
+	invalidToken := "abcdefghijklmnopqrstuvwxyz"
+	_, err := AuthenticateByToken(invalidToken)
+	assert.Error(t, err, "invalid token should return an error")
+	switch err.(type) {
+	case *TokenTooLongError:
+		// this is the one we want
+	default:
+		// all other error types are caught here, so fail if it gets to here
+		t.Fail()
+	}
+
+}
+
+func TestAuthenicateWithInvalidToken(t *testing.T) {
+
+	invalidToken := "abc"
+	_, err := AuthenticateByToken(invalidToken)
+	assert.Error(t, err, "invalid token should return an error")
+	switch err.(type) {
+	case *UnauthorisedError:
+		// this is the one we want
+	default:
+		// all other error types are caught here, so fail if it gets to here
+		t.Fail()
+	}
+}
